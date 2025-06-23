ad8b5ecb7e7da02cb5be5d7e1c8e4beeda7831e4
m-nasr-1988 <eng.m.nasr.1988@gmail.com>
2025-06-23 14:05:23 +0100

    Update Search_Entity.py

--- Search_Entity.py
+++ Search_Entity.py
@@ -40,10 +40,9 @@ def get_entity_name(entity_number, driver):
 def setup_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/chromium"
     return webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
 
 def main():
