207673ea7593adb6c845bc44d8e00339e3c58034
m-nasr-1988 <eng.m.nasr.1988@gmail.com>
2025-06-30 15:42:36 +0100

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.write("Secrets loaded:", st.secrets["CRO_API"])
-st.write("Email:", st.secrets["CRO_API"]["email"])
-st.write("API Key:", st.secrets["CRO_API"]["api_key"])
